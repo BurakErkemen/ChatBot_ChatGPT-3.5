17e7e2e624ebfbe2a2dd5b0fe2ca67199be2ed5d
BurakErkemen <burakfurkanerkemen@gmail.com>
2024-01-03 04:41:10 +0300

    update main.py
    Streamlit over

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-import pandas as pd 
+import os
+import streamlit as st
 import openai
 
+api_key = os.environ.get("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")
+
+#streamlit düzenlemesi
+st.title("FU Bot")
+
+if "message" not in st.session_state:
+    st.session_state.message=[]
+
+for message in st.session_state.message:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("")
+
+if prompt:
+    with st.chat_message(name="user"):
+        st.markdown(prompt)
+    st.session_state.message.append({"role":"user","content":prompt})
+
+    response = f"FU Bot: {prompt}"
+
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    
+    st.session_state.message.append({"role":"assistant","content":response})
+
+#with st.chat_message(name="Assistant"):
+#    st.write("Hello!!")
